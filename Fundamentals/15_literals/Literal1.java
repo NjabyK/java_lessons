dbefae2f47a2f421b969391e09a00c06c0e7c55e
Njabulo Kubheka <216016277@student.uj.ac.za>
2024-04-04 14:17:49 +0200

    literals

---
+++ Fundamentals/15_literals/Literal1.java
@@ -0,0 +1,13 @@
+class Literal1{
+	
+	public static void main(String[] args){
+		
+		int deciLiteral = 10;
+		int octalLiteral = 010;
+		int hexLiteral = 0xA;
+		int calculation = deciLiteral*octalLiteral*hexLiteral;
+		System.out.println("The result of the calculation: " + calculation);
+		
+	}
+	
+}
